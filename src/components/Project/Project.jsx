fee497b578a7030a531692c3dc2902938d4975a6
Armen Chakhalyan <armen@achakhalian.local>
2017-12-26 10:30:11 +0300

    fix

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -9,14 +9,14 @@ import cn from 'classnames';
 import s from './Project.styl';
 
 const buttonText = {
-	0: "Закрыть проект",
-	1: "Открыть проект"
-}
+	0: 'Закрыть проект',
+	1: 'Открыть проект'
+};
 
 export default class Project extends Component {
 	state = {
 		expanded: false,
-		showPopup: false,
+		showPopup: false
 	};
 
 	handleExpand = () => this.setState({
@@ -54,7 +54,7 @@ export default class Project extends Component {
 	}
 
 	render() {
-		const { title, vacancies, status, id, closeVacancy, openVacancy, deleteVacancy, addVacancy } = this.props;
+		const { title, vacancies, status, id, closeVacancy, openVacancy, deleteVacancy } = this.props;
 		const count = this.props.count || vacancies.length;
 		const { expanded, showPopup } = this.state;
 
@@ -62,22 +62,22 @@ export default class Project extends Component {
 			<div className={cn(s.block, {[s.expanded]: expanded})}>
 				{
 					showPopup &&
-						<Popup title="Новая вакансия" onClose={this.handleClosePopup}>
+						<Popup title='Новая вакансия' onClose={this.handleClosePopup}>
 							<Add {...{ onCreate: this.handleCreateVacancy, placeholder: 'Название вакансии' }} />
 						</Popup>
 				}
 
 				<Card {...{ expanded }}>
 					<div className={s.content}>
-						<h2 className={cn(s.title,{ [s.close]: !!status})} onClick={this.handleExpand}>{title}</h2>
+						<h2 className={cn(s.title, {[s.close]: !!status})} onClick={this.handleExpand}>{title}</h2>
 
 						<div className={s.subtitle}>
 							<div className={s.left}>
 								<span className={s.count}>{declOfVacancy(count)}</span>
 								{
 									status
-									? <div className={s.done}>Проект закрыт, сотрудники наняты</div>
-									: <FlatButton accent onClick={this.handleOpenPopup}>Добавить вакансию</FlatButton>
+										? <div className={s.done}>Проект закрыт, сотрудники наняты</div>
+										: <FlatButton accent onClick={this.handleOpenPopup}>Добавить вакансию</FlatButton>
 								}
 							</div>
 							<div className={s.right}>
@@ -87,7 +87,9 @@ export default class Project extends Component {
 								>
 									{buttonText[status]}
 								</FlatButton>
-								<FlatButton onClick={this.handleDelete}>Удалить</FlatButton>
+								<div className={s.delete}>
+									<FlatButton onClick={this.handleDelete}>Удалить</FlatButton>
+								</div>
 							</div>
 						</div>
 						<ListOfVacancies {...{ vacancies, expanded, id, closeVacancy, openVacancy, deleteVacancy }}/>
